8262c3c921aba75fd038be3669341d8ced35a4b8
Wissanukhong <wissanu.khong@gmail.com>
2020-06-18 07:17:54 +0700

    practise statement

--- 1-FallingSnow/conditionStatement.js
+++ 1-FallingSnow/conditionStatement.js
@@ -2,7 +2,18 @@ let a = 5;
 let b = a;
 
 if (a > 5) {
-    console.log(`ค่าของ ${b} มีค่ามากกว่า 5`)
+  console.log(`ค่าของ ${b} มีค่ามากกว่า 5`);
 } else {
-    console.log(`ค่าของ ${b} มีค่าน้อยกว่า 5`)
+  console.log(`ค่าของ ${b} มีค่าน้อยกว่า 5`);
 }
+
+// practise condition statement 
+function myGrade(grade) {
+  if ((grade > 80) & (grade <= 100)) {
+    console.log(`Your grade is "A"`);
+  } else if ((grade > 70) & (grade <= 80)) {
+    console.log(`Your grade is "B"`);
+  }
+}
+
+myGrade(80);
